53f158edd36db52ccb16bbf415893f08cbc14b9e
Steffen Knoblauch <knoblauch.steffen@web.de>
2020-09-04 20:21:54 +0200

    Project Setup

---
+++ premake5.lua
@@ -0,0 +1,38 @@
+workspace "SimpleRayTracer"
+	architecture "x64"
+	startproject "SimpleRayTracer"
+
+	configurations
+	{
+		"Debug",
+		"Release"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "SimpleRayTracer"
+	location "SimpleRayTracer"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/**.h",
+		"src/**.cpp"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
