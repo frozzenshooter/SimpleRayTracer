7ea4a57c4bf30017e248c42b37217a39bb191737
Steffen Knoblauch <knoblauch.steffen@web.de>
2020-09-05 09:37:41 +0200

    Generate png with file

--- SimpleRayTracer/src/main.cpp
+++ SimpleRayTracer/src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+#define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
 int main() {
@@ -8,23 +9,34 @@ int main() {
 
     const int image_width = 256;
     const int image_height = 256;
+    #define CHANNEL_NUM 3
 
     // Render
 
-   
-   for (int j = image_height - 1; j >= 0; --j) {
+    auto size = image_width * image_height * 3;
+
+    unsigned char* data = new unsigned char[size];
+
+    int index = 0;
+    for (int j = image_height - 1; j >= 0; --j) {
         for (int i = 0; i < image_width; ++i) {
-            auto r = double(i) / (image_width - 1);
-            auto g = double(j) / (image_height - 1);
+
+            auto r = double(i) / ((double)image_width - 1);
+            auto g = double(j) / ((double)image_height - 1);
             auto b = 0.25;
 
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
+            int ir = static_cast<unsigned char>(255.999 * r);
+            int ig = static_cast<unsigned char>(255.999 * g);
+            int ib = static_cast<unsigned char>(255.999 * b);
 
+            data[index++] = ir;
+            data[index++] = ig;
+            data[index++] = ib;
         }
     }
 
+    stbi_write_png("result.png", image_width, image_height, CHANNEL_NUM, data, image_width * CHANNEL_NUM);
 
-
+    delete [] data;
+    return 0;
 }
