941143d62a54141d37458a2ce44113f29233aafb
Steffen Knoblauch <knoblauch.steffen@web.de>
2020-09-06 11:24:37 +0200

    Fuzziness for metals introduced

--- SimpleRayTracer/src/geometry/Material.h
+++ SimpleRayTracer/src/geometry/Material.h
@@ -30,17 +30,18 @@ namespace SimpleRayTracer {
 
     class Metal : public Material {
     public:
-        Metal(const Color& a) : m_Albedo(a) {}
+        Metal(const Color& a, double f) : m_Albedo(a), m_Fuzz(f < 1 ? f : 1) {}
 
         virtual bool Scatter(const Ray& ray_in, const HitRecord& rec, Color& attenuation, Ray& scattered) const override
         {
             Vec3 reflected = Reflect(UnitVector(ray_in.Direction()), rec.Normal);
-            scattered = Ray(rec.Point, reflected);
+            scattered = Ray(rec.Point, reflected + m_Fuzz * RandomInUnitSphere());
             attenuation = m_Albedo;
             return (Dot(scattered.Direction(), rec.Normal) > 0);
         }
 
     public:
         Color m_Albedo;
+        double m_Fuzz;
     };
 }
